7c0ce0b9ee45eadae2e7ab1b31cbbfc88f6e6f11
rohitsigar <rohitsigar05@gmail.com>
2024-05-18 16:37:55 +0530

    UI changes

--- components/MainCarousel1.tsx
+++ components/MainCarousel1.tsx
@@ -14,40 +14,42 @@ import { Pagination, Navigation } from 'swiper/modules'
 
 const MainCarousel1: React.FC<> = () => {
   return (
-    <Swiper
-      style={{
-        '--swiper-navigation-color': '#1b2528',
-        '--swiper-pagination-color': '#1b2528',
-      }}
-      spaceBetween={50}
-      // slidesPerView={3}
-      slidesPerView={1}
-      loop={true}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Pagination, Navigation]}
-    >
-      <SwiperSlide>
-        <div className="w-full overflow-hidden">
-          <img
-            src="checking/checking2.png"
-            className="w-full"
-            alt="Full Width Image"
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="w-full overflow-hidden">
-          <img
-            src="checking/checking3.png"
-            className="w-full"
-            alt="Full Width Image"
-          />
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    <a href="/products">
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#1b2528',
+          '--swiper-pagination-color': '#1b2528',
+        }}
+        spaceBetween={50}
+        // slidesPerView={3}
+        slidesPerView={1}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+      >
+        <SwiperSlide>
+          <div className="w-full overflow-hidden">
+            <img
+              src="checking/checking2.png"
+              className="w-full"
+              alt="Full Width Image"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full overflow-hidden">
+            <img
+              src="checking/checking3.png"
+              className="w-full"
+              alt="Full Width Image"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </a>
   )
 }
 
--- components/videoCarousel/VideoCarousel.tsx
+++ components/videoCarousel/VideoCarousel.tsx
@@ -40,13 +40,13 @@ export default function VideoCarousel({}: {}) {
           class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
           aria-label="Slide 2"
         ></button>
-        <button
+        {/* <button
           type="button"
           data-te-target="#carouselExampleCaptions"
           data-te-slide-to="2"
           class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
           aria-label="Slide 3"
-        ></button>
+        ></button> */}
       </div>
 
       <div class="relative w-full overflow-hidden after:clear-both after:block after:content-[''] bg-primary">
