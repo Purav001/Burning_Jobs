e63ec5595a5e4712217b80c4c8ea2cce3087cd7f
Purav001 <puravsaluja4@gmail.com>
2024-07-15 22:00:01 +0530

    user profile page

---
+++ app/(front)/profile/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+import Form from './Form'
+
+export const metadata: Metadata = {
+title: 'Profile',
+description: 'Profile page to update information of user',
+}
+
+export default async function Profile() {
+return <Form />
+}
